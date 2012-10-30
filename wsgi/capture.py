40daafa7a4b46360300fe77ac3c28c7458718ac8
Angel Rivera <ariv3ra@gmail.com>
2012-10-30 18:39:36 -0400

    Add the initial files

---
+++ wsgi/capture.py
@@ -0,0 +1,121 @@
+import os
+import python_usdol
+import json
+import time
+import datetime
+from pymongo import Connection
+
+tkn = None   #Defines your token
+sct = None   #Defines your secret
+skipval = 0  #Defines the number of records to skip
+topval = 100 #Defines the max amount of records to retrieve
+wFlag = False #Defines the flag for the test inteh while loop
+num = 0
+DATASET = 'Safety/Fatalities'
+TABLE = 'OSHA_Fatalities'
+
+#Build the variables that use the assigned environment variables
+HOST = os.environ['OPENSHIFT_NOSQL_DB_HOST']
+PORT = int(os.environ['OPENSHIFT_NOSQL_DB_PORT'])
+DB_USER = os.environ['OPENSHIFT_NOSQL_DB_USERNAME']
+DB_PWD = os.environ['OPENSHIFT_NOSQL_DB_PASSWORD']
+DB_NAME = 'dol' #data base name
+
+dictFileCont = []  #Defines the content of the JSON data string
+dictMissingState = []
+
+#Setup the database connections
+mconn = Connection(HOST, PORT)
+db = mconn.dol
+
+#get DOL API tokens and secrets
+fl = open("config")  #Open the config file
+for f in fl:
+    ln = f.split("=")
+    if ln[0] == "token":
+        tkn = ln[1].strip()  #Assign the token value
+    else:
+        sct = ln[1].strip()  #Assign the secret value
+
+conn = python_usdol.Connection(tkn,sct)  #create an instance of the API connection
+
+def isData(ds,tb,topvalue, skipvalue):
+#Retrive data
+    row = conn.fetch_data(ds, tb, top=str(topvalue), skip=str(skipvalue), select='id')
+    #Check if there is data
+    if row:
+        return True
+    else:
+        return False
+
+# Pull data and check if there are elements to parse
+while isData(DATASET,TABLE,topval,skipval):
+    data = conn.fetch_data(DATASET, TABLE, top=str(topval), skip=str(skipval), orderby='id asc')
+    #data = conn.fetch_data('Safety/Fatalities', 'OSHA_Fatalities', orderby = 'id desc', top = 1, skip = 30)
+    #data = conn.fetch_data('Safety/Fatalities','OSHA_Fatalities')
+    #data = conn.fetch_data('Compliance/OSHA','full')
+
+    for d in data:
+        
+        c = d['dateofincident']
+        start = c.find("(")+1
+        end = c.find(")")
+        
+        ep = c[start:end]
+        
+        t = datetime.datetime.utcfromtimestamp(float(ep)/1000.)
+        #fmt = "%Y-%m-%d %H:%M:%S"
+        #fmt = "%Y-%m-%d"
+        
+        fmt = "%Y,%m,%d"
+        print "Time: " + str(t)
+        #d['dateofincident'] = t.strftime(fmt) # Update the date value
+        
+        d['dateofincident'] = t # Update the date value
+
+        ###
+        #  Here we are going to parse and separate the values of the companyandlocation field
+        ###
+        comp = d['companyandlocation'].strip().split(',')
+        
+        ## Separate the state and zip fields           
+        d['company'] = comp[0]  
+        
+        element = len(comp)-1  #Get the total number of elements minus 1 so it gets the last element
+        ch = comp[element].split()  #assign the state and zip values to ch as array
+        
+        print "The number of elements in company: " + str(len(comp)) 
+        
+        #print "check the failed item and it is = " + str(comp[element].split())
+        if ch:
+            print "####   The element is valid - length is:" + str(len(ch))
+            d["state"] = ch[0]
+            if len(ch) > 1:
+                d["zip"] = ch[1]
+        else:
+            print '####   The data is valid don\'t proceed'
+            dictMissingState.append(d)
+            d["state"] = "unknown"
+            
+        num += 1
+        ##Pop these fields from the row
+        d.pop('__metadata')
+        d.pop('companyandlocation')
+        # Build the JSON data set       
+        dictFileCont.append(d)
+        
+        print "Record: " + str(num) 
+        print d
+        
+    skipval += 100  # increment the skip value by 100
+'''
+#Dump the data to a file
+with open("oshafatals.txt","wb") as f:
+    json.dump(dictFileCont, f)
+    
+#Dump the records with incomplete or dirty company and state data    
+with open("oshafatalsMissingData.txt","wb") as f:
+    json.dump(dictMissingState, f)
+'''
+#Dump this to mongoDB
+db.fatals.insert(dictFileCont)
---
+++ wsgi/python_usdol.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+'''
+Python-USDOL
+
+Python module for access to the U.S. Department of Labor datasets via
+its developer API. (http://developer.dol.gov)
+'''
+
+__version__ = "1.0"
+__license__ = "BSD"
+__copyright__ = "Copyright 2011, Matt DeBoard"
+__author__ = "Matt DeBoard <http://mattdeboard.net>"
+
+
+import datetime
+import hashlib
+import hmac
+import json
+import string
+import urllib2
+
+USDOL_URL = 'http://api.dol.gov'
+API_VER = 'V1'
+
+
+class Connection(object):
+    """
+    An instance of Connection represents a connection to the U.S. Dept.
+    of Labor's developer API.
+
+    Connection(token, secret[, dataset, table])
+
+    'token' and 'secret' are the API token & shared secret is the API
+    token generated at http://developer.dol.gov. Complete listing of
+    valid datasets and tables are to be found at the same URL.
+
+    Once you have instantiated Connection, call the get_data method,
+    which will fetch the data according to parameters. For more info,
+    consult get_data's docstring.
+    
+    """
+    def __init__(self, token=None, secret=None):
+        self.token = token
+        self.secret = secret
+
+    def _urlencode(self, d):
+        ret = ['%s=%s' % (k, v) for k, v in d.iteritems()]
+        return '&'.join(ret)
+        
+    def _get_timestamp(self):
+        """
+        Returns the Timestamp and ApiKey portions of the query string.
+        
+        There is a 15-minute window for an auth string with a valid
+        timestamp to be accepted by the US DOL's servers.
+        
+        """
+        t = datetime.datetime.utcnow().replace(microsecond=0)
+        t -= datetime.timedelta(minutes=1)
+        return (t, t.isoformat()+'Z')
+
+    def _get_message(self, qs):
+        """
+        Generates the message to be hashed by HMAC SHA1 algorithm and
+        passed to the API in the "Authorization" header.
+
+        """
+        baseurl = '/%s/%s/' % (API_VER, self.dataset)
+        if self.table != '$metadata':
+            baseurl += self.table
+        baseurl += qs
+        date_time, timestamp = self._get_timestamp()
+        header_dict = {"Timestamp": timestamp, "ApiKey": self.token}
+        return (header_dict, '%s&%s' % (baseurl, self._urlencode(header_dict)))
+
+    def _get_header(self, qs):
+        d, message = self._get_message(qs)
+        h = hmac.new(self.secret, message, hashlib.sha1)
+        d['Signature'] = h.hexdigest()
+        return self._urlencode(d)
+
+    def _get_querystring(self, **kwargs):
+        """
+        Generates the query string which will be appended to the URL of
+        the target dataset/table, e.g.:
+
+        ?&top=10&skip=20
+
+        """
+        qs = []
+        for key, value in kwargs.items():
+            if value:
+                # `filter` is a keyword in Python, so I avoid using it by
+                # adding a _ to the end, then removing it here.
+                qs.append("$%s=%s" %
+                          (key.replace('_', ''), str(value).replace(' ', '+')))
+        return '?' + string.join(qs, '&')
+
+    def _get_request(self, qs='', fmt='json'):
+        url_args = [USDOL_URL, API_VER, self.dataset, self.table]
+        header = self._get_header(qs)
+        url = string.join(url_args, '/') + qs
+        req = urllib2.Request(url, headers={"Authorization": header,
+                                            "Accept": 'application/%s' % fmt})
+        return req
+
+    def fetch_data(self, dataset, table='$metadata', fmt='json', top=0,
+                   skip=0, select='', orderby='', filter_=''):
+        """
+        fetch_data(dataset, table[, fmt, top, skip, select, orderby]) ->
+        
+            Return an object representing the information in the specified
+            table from the specified dataset.
+
+            The rest of the args work as outlines in the DOL's API
+            reference (http://developer.dol.gov/html-req.htm).
+        
+        'fmt' is json by default. Valid choices are 'xml' and 'json'.
+        
+        """
+        
+        qs = self._get_querystring(top=top, skip=skip, select=select,
+                                   orderby=orderby, filter_=filter_)
+        self.dataset = dataset
+        self.table = table
+        enc_opts = ['json', 'xml']
+        if fmt not in enc_opts:
+            raise AttributeError("Valid format choices are: json, xml")
+        if table == '$metadata' and fmt != 'xml':
+            fmt = 'xml'
+    
+        urlstr = self._get_request(qs, fmt)
+        data = urllib2.urlopen(urlstr)
+        if fmt == 'json':
+            d = json.loads(data.read())['d']
+            #removed the .get() function from d below
+            ret = d#.get('results', d)
+        else:
+            ret = data.read()
+        return ret
